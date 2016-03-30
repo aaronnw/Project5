fe1e6fac084a3e96fe390d7effc10e7817a1e9ee
Aaron Wilson <aaronnw11@gmail.com>
2016-03-29 23:29:26 -0500

    Basic input reading added

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -1,4 +1,49 @@
 #include <iostream>
+using namespace std;
+
+/*
+template <class DT>  
+class ParentBinaryTree {
+	template<class T>
+	friend ostream& operator<< (ostream& s, ParentBinaryTree<T>& pbt); //Overloaded ostream operator
+protected:   
+	DT* ParentArray;
+	int numNodes;
+public:
+	ParentBinaryTree();
+	ParentBinaryTree(int size);
+	~ParentBinaryTree();
+	ParentBinaryTree(const ParentBinaryTree& pbt);
+	void operator=(const ParentBinaryTree<DT>& pbt);
+	int getSize();
+	int getHeight();
+	getLeft();
+	getRight();
+	preorderTraversal();
+	inorderTraversal();
+	postorderTraversal();
+};
+*/
+
 int main() {
+	int numNodes = 0;
+	int temp;
+	int root;
+	int left;
+	int right;
+	char c;
+	//Reads the number of nodes
+	cin >> numNodes;
+	cout << numNodes << endl;
+
+	//Reads the rest of the file
+	while (!cin.eof()){
+		//For each line
+		cin >> root;
+		cin >> left;
+		cin >> right;
+		cout << root << " " << left << " " << right << endl;
+		
+	}
 	return 0;
 }
