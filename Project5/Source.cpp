d1c14233008c780f1b107a4d90e49ae869fb1291
Aaron Wilson <aaronnw11@gmail.com>
2016-04-01 00:37:21 -0500

    before sync

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -1,4 +1,15 @@
 #include <iostream>
+
+template <class DT>
+class ParentBinaryTree {
+protected:
+	DT* ParentArray;
+	int numNodes;
+public:
+	//All the required methods
+};
+
+
 int main() {
 	return 0;
 }
