9a8f202d8eeb4f26e390d407f090aca3e98441b4
Aaron Wilson <aaronnw11@gmail.com>
2016-04-08 21:04:30 -0500

    Added comments and fixed errors

--- Project5/Source.cpp
+++ Project5/Source.cpp
@@ -7,29 +7,37 @@ class ParentBinaryTree {
 	template<class T>
 	friend ostream& operator<< (ostream& s, ParentBinaryTree<T>& pbt); //Overloaded ostream operator
 protected:   
-	DT* ParentArray;
-	DT* LeftRightArray;
-	int numNodes;
-	bool firstRoot = true;
+	DT* ParentArray; //Array indexed by each position with a value of each position's parent
+	int* LeftRightArray; //Array indexed by each position with a value of 0 for left nodes and 1 for right nodes
+	int numNodes; //Number of nodes in the tree
+	bool firstRoot = true; 
 public:
-	ParentBinaryTree();
-	ParentBinaryTree(int size);
-	~ParentBinaryTree();
-	ParentBinaryTree(const ParentBinaryTree& pbt);
-	void operator=(const ParentBinaryTree<DT>& pbt);
-	void insert(const DT & root, const DT & left, const DT & right);
-	int getSize();
-	int getHeight();
-	int getLeft(const DT& x);
-	int getRight(const DT& x);
-	bool isLeaf(const DT& x);
-	void preorderTraversal();
+	ParentBinaryTree(); //Default constructor
+	ParentBinaryTree(int size); //Initializer
+	~ParentBinaryTree(); //Destructor
+	ParentBinaryTree(ParentBinaryTree& pbt); //Copy constructor
+	void operator=(ParentBinaryTree<DT>& pbt); //Overloaded assignment operator
+	void insert(const DT & root, const DT & left, const DT & right); //Inserts the values to create the tree
+	int getSize(); //Returns the number of nodes in the tree
+	int getHeight(); //Returns the height of the tree
+	int getLeft(int x); //Returns the left child position of the given position
+	int getRight(int x);  //Returns the right child position of the given position
+	bool isLeaf(int x);  //Checks if the position given is a leaf node
+	void preorderTraversal(); 
 	void preorderTraversal(int x);
 	void inorderTraversal();
 	void inorderTraversal(int x);
 	void postorderTraversal();
 	void postorderTraversal(int x);
 };
+
+///Overloaded ostream operator
+template<class T>
+ostream& operator<< (ostream& s, ParentBinaryTree<T>& tree) {
+	tree.preorderTraversal();
+	s << endl;
+	return s;
+}
 
 ///Default Constructor
 template<class DT>
@@ -50,27 +58,32 @@ template<class DT>
 ParentBinaryTree<DT>::~ParentBinaryTree() {
 
 }
+///Copy constructor
 template<class DT>
-ParentBinaryTree<DT>::ParentBinaryTree(const ParentBinaryTree & pbt) {
+ParentBinaryTree<DT>::ParentBinaryTree(ParentBinaryTree<DT> & pbt) {
+	numNodes = pbt.numNodes;
+	ParentArray = new DT[numNodes];
+	LeftRightArray = new int[numNodes];
 	//Copy the parent array
 	for (int i = 0; i < pbt.getSize(); i++) {
 		ParentArray[i] = pbt.ParentArray[i];
-	}
-	for (int i = 0; i < pbt.getSize(); i++) {
 		LeftRightArray[i] = pbt.LeftRightArray[i];
 	}
-	numNodes = pbt.numNodes;
+	
 }
+///Overloaded assignment operator
 template<class DT>
-void ParentBinaryTree<DT>::operator=(const ParentBinaryTree<DT>& pbt) {
+void ParentBinaryTree<DT>::operator=(ParentBinaryTree<DT>& pbt) {
+	numNodes = pbt.numNodes;
+	ParentArray = new DT[numNodes];
+	LeftRightArray = new int[numNodes];
+	//Copy the parent array
 	for (int i = 0; i < pbt.getSize(); i++) {
 		ParentArray[i] = pbt.ParentArray[i];
-	}
-	for (int i = 0; i < pbt.getSize(); i++) {
 		LeftRightArray[i] = pbt.LeftRightArray[i];
 	}
-	numNodes = pbt.numNodes;
 }
+///Inserts values into the tree
 template<class DT>
 void ParentBinaryTree<DT>::insert(const DT & root, const DT & left, const DT & right) {
 	if (firstRoot) {
@@ -113,9 +126,9 @@ int ParentBinaryTree<DT>::getHeight() {
 	}
 	return maxHeight;
 }
-
+///Returns the left child of a given position
 template<class DT>
-int ParentBinaryTree<DT>::getLeft(const DT & x) {
+int ParentBinaryTree<DT>::getLeft(int x) {
 	//Search through parent array for the value x to find it's children
 	for (int i = 0; i < numNodes; i++) {
 		if (ParentArray[i] == x) {
@@ -126,9 +139,9 @@ int ParentBinaryTree<DT>::getLeft(const DT & x) {
 	}
 	return -1;
 }
-
+///Returns the right child of a given position
 template<class DT>
-int ParentBinaryTree<DT>::getRight(const DT & x) {
+int ParentBinaryTree<DT>::getRight(int x) {
 	//Search through parent array for the value x to find it's children
 	for (int i = 0; i < numNodes; i++) {
 		if (ParentArray[i] == x) {
@@ -139,20 +152,20 @@ int ParentBinaryTree<DT>::getRight(const DT & x) {
 	}
 	return -1;
 }
-
+///Returns a boolean true if the position is a leaf node
 template<class DT>
-bool ParentBinaryTree<DT>::isLeaf(const DT & x) {
+bool ParentBinaryTree<DT>::isLeaf(int x) {
+	//Check if the position has children
 	if ((getLeft(x) == -1 ) && (getRight(x) == -1)) {
 		return true;
 	} else {
 		return false;
 	}
 }
-
+///Prints the node values in preorder format
 template<class DT>
 void ParentBinaryTree<DT>::preorderTraversal(int x) {
 	//Root, left, right
-	//Find root node
 	cout << x;
 	if (getLeft(x) != -1) {
 		preorderTraversal(getLeft(x));
@@ -161,6 +174,7 @@ void ParentBinaryTree<DT>::preorderTraversal(int x) {
 		preorderTraversal(getRight(x));
 	}
 }
+///Finds the root node then prints the node values in preorder format
 template<class DT>
 void ParentBinaryTree<DT>::preorderTraversal() {
 	//Root, left, right
@@ -169,13 +183,13 @@ void ParentBinaryTree<DT>::preorderTraversal() {
 	while (ParentArray[root] != -1 && root < numNodes) {
 		root++;
 	}
+	//Start the traversal at the root node
 	preorderTraversal(root);
 }
-
+//Prints the node values in inorder format
 template<class DT>
 void ParentBinaryTree<DT>::inorderTraversal(int x) {
 	//Left, root, right
-	//Find root node
 	if (getLeft(x) != -1) {
 		inorderTraversal(getLeft(x));
 	}
@@ -184,6 +198,7 @@ void ParentBinaryTree<DT>::inorderTraversal(int x) {
 		inorderTraversal(getRight(x));
 	}
 }
+///Finds the root node then prints the node values in inorder format
 template<class DT>
 void ParentBinaryTree<DT>::inorderTraversal() {
 	//Left, root, right
@@ -192,13 +207,13 @@ void ParentBinaryTree<DT>::inorderTraversal() {
 	while (ParentArray[root] != -1 && root < numNodes) {
 		root++;
 	}
+	//Start the traversal at the root node
 	inorderTraversal(root);
 }
-
+///Prints the node values in postorder format
 template<class DT>
 void ParentBinaryTree<DT>::postorderTraversal(int x) {
 	//Left, root, right
-	//Find root node
 	if (getLeft(x) != -1) {
 		postorderTraversal(getLeft(x));
 	}
@@ -207,6 +222,7 @@ void ParentBinaryTree<DT>::postorderTraversal(int x) {
 	}
 	cout << x;
 }
+///Finds the root node then prints the node values in postorder format
 template<class DT>
 void ParentBinaryTree<DT>::postorderTraversal() {
 	//Left, root, right
@@ -215,9 +231,9 @@ void ParentBinaryTree<DT>::postorderTraversal() {
 	while (ParentArray[root] != -1 && root < numNodes) {
 		root++;
 	}
+	//Start the traversal at the root node
 	postorderTraversal(root);
 }
-
 
 int main() {
 	int numNodes = 0;
@@ -227,21 +243,37 @@ int main() {
 	//Reads the number of nodes
 	cin >> numNodes;
 	cout << numNodes << endl;
+	//Initializer
 	ParentBinaryTree<int>* tree = new ParentBinaryTree<int>(numNodes);
 	//Reads the rest of the file
 	while (!cin.eof()){
-		//For each line
+		//For each line add the info to the tree
 		cin >> root;
 		cin >> left;
 		cin >> right;
 		tree->insert(root, left, right);
 	}
+	//Show that all the methods work
+	//Default constructor
+	ParentBinaryTree<int>* emptyTree = new ParentBinaryTree<int>();
+	//Copy constructor
+	ParentBinaryTree<int>* newTree = new ParentBinaryTree<int>(*tree);
+	//Overloaded assignment operator
+	ParentBinaryTree<int>* tree2 = tree;
+	//size method
+	cout << tree->getSize() << endl;
+	//height method
 	cout << tree->getHeight() << endl;
+	//Preorder
 	tree->preorderTraversal();
 	cout << endl;
+	//Inorder
 	tree->inorderTraversal();
 	cout << endl;
+	//Postorder
 	tree->postorderTraversal();
 	cout << endl;
+	cout << *tree;
+	//getRight and getLeft included in traversals
 	return 0;
 }
